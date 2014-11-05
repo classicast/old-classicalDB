36047877a0965880833ec57e37b80de27e93181f
Andrew Zey <andrew@andrewzey.com>
2014-11-05 15:11:10 -0800

    (bug) fix morgan install on heroku

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var logger = require('morgan');
 
 var app = express();
 app.set('port', (process.env.PORT || 5000))
 
 //Load Middleware
 app.use(express.static(__dirname + '/public')); //Serve static files
-if (process.env.NODE_ENV !== 'production') {
-  var logger = require('morgan');
-  app.use(logger('short')); //Log all requests except static file requests
-}
+app.use(logger('short')); //Log all requests except static file requests
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 
