1631e15404029fc14a5a806778800dd5b0a31ae6
Andrew Zey <andrew@andrewzey.com>
2014-11-05 15:06:38 -0800

    (bug) make morgan only run in non-production

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var logger = require('morgan');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 app.set('port', (process.env.PORT || 5000))
 
 //Load Middleware
 app.use(express.static(__dirname + '/public')); //Serve static files
-app.use(logger('short')); //Log all requests except static file requests
+if (process.env.NODE_ENV !== 'production') {
+  var logger = require('morgan');
+  app.use(logger('short')); //Log all requests except static file requests
+}
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 
