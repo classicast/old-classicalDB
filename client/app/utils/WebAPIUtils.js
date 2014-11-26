44b2983bbb5038c821e38346a30b0eb31dc62aa8
Carolyn Moss <caly.moss@gmail.com>
2014-11-25 18:06:40 -0800

    (feat) clean up client components and implement super agent for http requests

--- client/app/utils/WebAPIUtils.js
+++ client/app/utils/WebAPIUtils.js
@@ -1,27 +1,30 @@
-'use strict';
-
 var ServerActionCreators = require('../actions/ServerActionCreators');
+var superagent = require('superagent');
 
 module.exports = {
 
   getAllLabels: function() {
-    var labels;
-    ServerActionCreators.receiveLabels(labels);
+    request
+    .get('api/labels')
+    .end(function(error, res){
+      // ServerActionCreators.receiveLabels(res);
+      if(error) {
+        throw error;
+      }
+      console.log(res);
+    });
   },
 
-  // createLabel: function(label) {
-  //   // post to server/db
-  //   $.ajax({
-  //     type: 'POST',
-  //     url: '../../index.js',
-  //     data: label,
-  //     success: function() {
-  //       console.log('success!');
-  //       ServerActionCreators.receiveCreatedLabel(label);
-  //     },
-  //     error: function() {
-  //       console.log('error!');
-  //     }
-  //   });
-  // }
+  createLabel: function(label) {
+    // post to server/db
+   request
+    .post('api/labels')
+    .end(function(error, res){
+      if(error) {
+        throw error;
+      }
+      console.log("success!", res);
+    });
+  }
+
 };
